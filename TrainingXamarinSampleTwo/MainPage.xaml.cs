e8d28a5a9bace88aeac9ea482fafedcbf77db408
Anjani B M <anjani.b-m@dxc.com>
2024-02-13 13:10:01 +0530

    Add project files.

---
+++ TrainingXamarinSampleTwo/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TrainingXamarinSampleTwo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();// Constructor for the main page
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+           label1.Text = fName.Text + "\t"+ lName.Text;
+        }
+    }
+}
---
+++ TrainingXamarinSampleTwo/PostPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TrainingXamarinSampleTwo
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PostPage : ContentPage
+	{
+		public PostPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            // Post
+
+            label1.Text = fName.Text + " " + lName.Text + " " + comments.Text;
+
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            //Clear
+            fName.Text = null;
+            lName.Text = null;
+            comments.Text = null;
+        }
+    }
+}
